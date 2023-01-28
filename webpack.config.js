c11f8f232fd29f8227620caa9a5dedbd3dd3643c
Phan Xuan Giang <giang.phan@elca.vn>
2023-01-29 00:45:02 +0700

    Initialize project.

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+    entry: { bundle: path.resolve(__dirname, 'src/index.js') },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'build'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(css)$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Photo Exhibition App',
+            filename: 'index.html',
+            template: 'public/index.html',
+        }),
+        new BundleAnalyzerPlugin(),
+    ],
+}
