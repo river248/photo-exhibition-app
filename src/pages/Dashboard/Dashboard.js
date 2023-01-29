b94906c50bd3111067fc77b5a9f3578b408e433a
Phan Xuan Giang <giang.phan@elca.vn>
2023-01-30 02:20:01 +0700

    build header #2

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -18,20 +18,30 @@ function Dashboard() {
             <div className={cx('header')}>
                 <form id={formId} className={cx('query-form')} onSubmit={(e) => handleSubmit(e)}>
                     <div className={cx('left-container-form')}>
-                        <CustomInput label={'Before: '} />
-                        <CustomInput label={'when: '} />
+                        <div className={cx('search-inp')}>
+                            <CustomInput label={'Before:'} />
+                        </div>
+                        <div className={cx('time-inp')}>
+                            <CustomInput label={'when:'} />
+                        </div>
                     </div>
 
                     <div className={cx('middle-container-form')}>
-                        <CustomInput label={'Find: '} />
+                        <CustomInput className={cx('query-inp')} label={'Find:'} />
                     </div>
 
                     <div className={cx('right-container-form')}>
-                        <CustomInput label={'After: '} />
-                        <CustomInput label={'when: '} />
+                        <div className={cx('search-inp')}>
+                            <CustomInput label={'After:'} />
+                        </div>
+                        <div className={cx('time-inp')}>
+                            <CustomInput label={'when:'} />
+                        </div>
                     </div>
 
-                    {/* <input type={'submit'} /> */}
+                    <div className={cx('submit-inp-wrapper')}>
+                        <input className={cx('submit-inp')} type={'submit'} />
+                    </div>
                 </form>
             </div>
         </div>
