cc360d7b96c5a4af962bd72c5156a41a6c7eb87b
Phan Xuan Giang <giang.phan@elca.vn>
2023-01-30 12:37:31 +0700

    handle logic header

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useId } from 'react'
+import React, { useId, useRef } from 'react'
 import classNames from 'classnames/bind'
 import { faTurnDown } from '@fortawesome/free-solid-svg-icons'
 
@@ -10,9 +10,25 @@ const cx = classNames.bind(styles)
 
 function Dashboard() {
     const formId = useId()
+    const beforeInpRef = useRef()
+    const queryInpRef = useRef()
+    const afterInpRef = useRef()
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        let data = {
+            before: beforeInpRef.current.value,
+            after: afterInpRef.current.value,
+        }
+
+        // Handle submit if query is not null
+        if (queryInpRef.current.value) {
+            data = {
+                ...data,
+                query: queryInpRef.current.value,
+            }
+            console.log(data)
+        }
     }
 
     return (
@@ -20,17 +36,16 @@ function Dashboard() {
             <div className={cx('header')}>
                 <form id={formId} className={cx('query-form')} onSubmit={(e) => handleSubmit(e)}>
                     <div className={cx('left-container-form')}>
-                        <Input className={cx('search-inp')} label={'Before:'} />
+                        <Input ref={beforeInpRef} className={cx('search-inp')} label={'Before:'} />
                         <Input className={cx('time-inp')} label={'when:'} />
                     </div>
 
                     <div className={cx('middle-container-form')}>
-                        <Input className={cx('query-inp')} label={'Find:'} />
+                        <Input ref={queryInpRef} className={cx('query-inp')} label={'Find:'} />
                     </div>
 
                     <div className={cx('right-container-form')}>
-                        <Input className={cx('search-inp')} label={'After:'} />
-
+                        <Input ref={afterInpRef} className={cx('search-inp')} label={'After:'} />
                         <Input className={cx('time-inp')} label={'when:'} />
                     </div>
 
