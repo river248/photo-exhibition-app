687c1c1add3b7de8e81562399d9841ad4ae4e3be
Phan Xuan Giang <giang.phan@elca.vn>
2023-02-06 22:03:00 +0700

    images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,29 @@
 const images = {
     noImage: require('~/assets/images/noImage.jpg'),
+    img05_075336: require('~/assets/images/image_retrieval_image/201902/05/20190205_075336_000.jpg'),
+    img05_075408: require('~/assets/images/image_retrieval_image/201902/05/20190205_075408_000.jpg'),
+    img05_075440: require('~/assets/images/image_retrieval_image/201902/05/20190205_075440_000.jpg'),
+    img05_075941: require('~/assets/images/image_retrieval_image/201902/05/20190205_075941_000.jpg'),
+    img05_082810: require('~/assets/images/image_retrieval_image/201902/05/20190205_082810_000.jpg'),
+    img05_083402: require('~/assets/images/image_retrieval_image/201902/05/20190205_083402_000.jpg'),
+    img05_091011: require('~/assets/images/image_retrieval_image/201902/05/20190205_091011_000.jpg'),
+    img05_094120: require('~/assets/images/image_retrieval_image/201902/05/20190205_094120_000.jpg'),
+    img05_102720: require('~/assets/images/image_retrieval_image/201902/05/20190205_102720_000.jpg'),
+    img05_102752: require('~/assets/images/image_retrieval_image/201902/05/20190205_102752_000.jpg'),
+    img05_102824: require('~/assets/images/image_retrieval_image/201902/05/20190205_102824_000.jpg'),
+
+    img06_075312: require('~/assets/images/image_retrieval_image/201902/06/20190206_075312_000.jpg'),
+    img06_075344: require('~/assets/images/image_retrieval_image/201902/06/20190206_075344_000.jpg'),
+    img06_075416: require('~/assets/images/image_retrieval_image/201902/06/20190206_075416_000.jpg'),
+    img06_075624: require('~/assets/images/image_retrieval_image/201902/06/20190206_075624_000.jpg'),
+    img06_075800: require('~/assets/images/image_retrieval_image/201902/06/20190206_075800_000.jpg'),
+    img06_080112: require('~/assets/images/image_retrieval_image/201902/06/20190206_080112_000.jpg'),
+    img06_081653: require('~/assets/images/image_retrieval_image/201902/06/20190206_081653_000.jpg'),
+    img06_084523: require('~/assets/images/image_retrieval_image/201902/06/20190206_084523_000.jpg'),
+    img06_084555: require('~/assets/images/image_retrieval_image/201902/06/20190206_084555_000.jpg'),
+    img06_084803: require('~/assets/images/image_retrieval_image/201902/06/20190206_084803_000.jpg'),
+    img06_085011: require('~/assets/images/image_retrieval_image/201902/06/20190206_085011_000.jpg'),
+    img06_085043: require('~/assets/images/image_retrieval_image/201902/06/20190206_085043_000.jpg'),
 }
 
 export default images
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import classNames from 'classnames/bind'
 
 import styles from './Dashboard.module.scss'
@@ -8,81 +8,80 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark, faSquareCheck } from '@fortawesome/free-solid-svg-icons'
 import Button from '~/components/Button'
 import routes from '~/configs/routes'
+import images from '~/assets/images'
 
 const cx = classNames.bind(styles)
 
 const listImage = [
-    { focused: true, number: ['before', 'current', 'after'] },
-    { focused: true, number: ['before', 'current', 'after'] },
-    { focused: true, number: ['before', 'current', 'after'] },
-    { focused: true, number: ['before', 'current', 'after'] },
-    { focused: true, number: ['before', 'current', 'after'] },
-    { focus: false, number: ['', 'current', 'after'] },
-    { focus: false, number: ['before', 'current', 'after'] },
-    { focus: false, number: ['before', 'current', 'after'] },
-    { focus: false, number: ['before', 'current', 'after'] },
-    { focus: false, number: ['before', 'current', 'after'] },
-    { focus: false, number: ['before', 'current', 'after'] },
-    { focus: false, number: ['', 'current', 'after'] },
+    { focused: true, image: [images.img05_075336, images.img05_075408, images.img05_075440] },
+    { focused: true, image: [images.img05_075941, images.img05_082810, images.img05_083402] },
+    { focused: true, image: [images.img05_091011, images.img05_094120, images.img05_102720] },
+    { focused: true, image: [images.img05_102752, images.img05_102824, ''] },
+    { focused: false, image: [images.img06_075312, images.img06_075344, images.img06_075416] },
+    { focused: false, image: [images.img06_075624, images.img06_075800, images.img06_080112] },
+    { focused: false, image: [images.img06_081653, images.img06_084523, images.img06_084555] },
+    { focused: false, image: [images.img06_084803, images.img06_085011, images.img06_085043] },
 ]
 
 function Dashboard() {
     return (
-        <div className={cx('wrapper')}>
+        <Fragment>
             <Filter />
-            <div className={cx('container', 'expand')}>
+            <div className={cx('wrapper', 'expand')}>
                 <div className={cx('header')}>
                     <Button className={cx('link')} to={routes.savedScence} title={'Saved scence'} />
                     <h1>Result</h1>
                 </div>
-                {listImage.map((item, index) => (
-                    <div key={index} className={cx('gallery', item.focused ? 'border-image' : '')}>
-                        <div className={cx('image-wrapper', 'image-wrapper-left')}>
-                            {item.number[0] && (
-                                <OverlayImage src={''} alt={''}>
-                                    <div className={cx('img-action')}>
-                                        <span className={cx('icon')}>
-                                            <FontAwesomeIcon icon={faBookmark} />
-                                        </span>
-                                        <span className={cx('icon')}>
-                                            <FontAwesomeIcon icon={faSquareCheck} />
-                                        </span>
-                                    </div>
-                                </OverlayImage>
-                            )}
+                <div className={cx('container')}>
+                    {listImage.map((item, index) => (
+                        <div key={index} className={cx('gallery', item.focused ? 'border-image' : '')}>
+                            <div className={cx('image-wrapper', 'image-wrapper-left')}>
+                                {item.image[0] && (
+                                    <OverlayImage src={item.image[0]} alt={''}>
+                                        <div className={cx('img-action')}>
+                                            <span className={cx('icon')}>
+                                                <FontAwesomeIcon icon={faBookmark} />
+                                            </span>
+                                            <span className={cx('icon')}>
+                                                <FontAwesomeIcon icon={faSquareCheck} />
+                                            </span>
+                                        </div>
+                                    </OverlayImage>
+                                )}
+                            </div>
+                            <div className={cx('image-wrapper', 'image-wrapper-mid', item.focused ? 'box-shadow' : '')}>
+                                {item.image[1] && (
+                                    <OverlayImage src={item.image[1]} alt={''}>
+                                        <div className={cx('img-action')}>
+                                            <span className={cx('icon')}>
+                                                <FontAwesomeIcon icon={faBookmark} />
+                                            </span>
+                                            <span className={cx('icon')}>
+                                                <FontAwesomeIcon icon={faSquareCheck} />
+                                            </span>
+                                        </div>
+                                    </OverlayImage>
+                                )}
+                            </div>
+                            <div className={cx('image-wrapper', 'image-wrapper-right')}>
+                                {item.image[2] && (
+                                    <OverlayImage src={item.image[2]} alt={''}>
+                                        <div className={cx('img-action')}>
+                                            <span className={cx('icon')}>
+                                                <FontAwesomeIcon icon={faBookmark} />
+                                            </span>
+                                            <span className={cx('icon')}>
+                                                <FontAwesomeIcon icon={faSquareCheck} />
+                                            </span>
+                                        </div>
+                                    </OverlayImage>
+                                )}
+                            </div>
                         </div>
-                        <div className={cx('image-wrapper', 'image-wrapper-mid', item.focused ? 'box-shadow' : '')}>
-                            {item.number[1] && (
-                                <OverlayImage src={''} alt={''}>
-                                    <div className={cx('img-action')}>
-                                        <span className={cx('icon')}>
-                                            <FontAwesomeIcon icon={faBookmark} />
-                                        </span>
-                                        <span className={cx('icon')}>
-                                            <FontAwesomeIcon icon={faSquareCheck} />
-                                        </span>
-                                    </div>
-                                </OverlayImage>
-                            )}
-                        </div>
-                        <div className={cx('image-wrapper', 'image-wrapper-right')}>
-                            {item.number[2] && (
-                                <OverlayImage src={''} alt={''}>
-                                    <div className={cx('img-action')}>
-                                        <span className={cx('icon')}>
-                                            <FontAwesomeIcon icon={faBookmark} />
-                                        </span>
-                                        <span className={cx('icon')}>
-                                            <FontAwesomeIcon icon={faSquareCheck} />
-                                        </span>
-                                    </div>
-                                </OverlayImage>
-                            )}
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
+        </Fragment>
     )
 }
 
